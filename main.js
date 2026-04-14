8125148236dc0135b60a1a0fa1b53060c22e3a63
yeg147 <yagneshbadheka147@gmail.com>
2026-04-14 23:57:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
 if (require('electron-squirrel-startup')) app.quit();
@@ -9,12 +9,20 @@ function createWindow() {
     height: 800,
     autoHideMenuBar: true,
     webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true
+      nodeIntegration: true, // Changed to true
+      contextIsolation: false // Changed to false for simpler printing
     }
   });
 
   win.loadFile('index.html');
+
+  // This part handles the print command from your web app
+  ipcMain.on('print-to-pdf', (event) => {
+    const winToPrint = BrowserWindow.fromWebContents(event.sender);
+    winToPrint.webContents.print({}, (success, failureReason) => {
+      if (!success) console.log(failureReason);
+    });
+  });
 }
 
 app.whenReady().then(createWindow);
